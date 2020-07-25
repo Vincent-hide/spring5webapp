275e525acb7d0811153d40c6c75882a2247a0720
Vincent-hide <hidehiro.okayama@gmail.com>
2020-07-25 16:00:17 -0400

    thymeleaf template engine connected

--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -27,6 +27,10 @@ public class Book {
         this.isbn = isbn;
     }
 
+    public Long getId() { return id; }
+
+    public void setId(Long id) { this.id = id; }
+
     public String getTitle() {
         return title;
     }
